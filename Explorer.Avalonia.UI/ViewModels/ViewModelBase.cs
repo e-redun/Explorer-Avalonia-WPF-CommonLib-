3ce7524a64d4249c9b85fa1492afe98f93093367
redunea <redunea@IN-001232>
2023-01-11 10:50:27 +0500

    WindowStartupLocation="CenterScreen"

---
+++ Explorer.Avalonia.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Explorer.Avalonia.UI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
